4f38a65f1aacfa2883ea1046dcc29d4a30e1106d
Uladzislava <santalova.lada25@gmail.com>
2024-10-31 21:18:01 +0100

    feat: implement 4th task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { join } from 'path';
+import { access, rename as fsRename } from 'fs/promises';
+
+const isPathExist = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const rename = async () => {
-    // Write your code here 
+  const oldName = join(import.meta.dirname, 'files', 'wrongFilename.txt');
+  const newName = join(import.meta.dirname, 'files', 'properFilename.md');
+
+  const isRenamed =
+    !(await isPathExist(oldName)) || (await isPathExist(newName));
+
+  if (isRenamed) {
+    throw new Error('FS operation failed');
+  } else {
+    fsRename(oldName, newName);
+  }
 };
 
 await rename();
