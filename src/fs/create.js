6530bae9ba6cb46dc57690e10dd3fb8f0938ef34
Uladzislava <santalova.lada25@gmail.com>
2024-10-31 18:16:22 +0100

    feat: implement 1st task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile, access, constants } from 'node:fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, 'files', 'fresh.txt');
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      writeFile(filePath, 'I am fresh and young', (err) => {
+        if (err) throw new Error(err);
+      });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await create();
