c253fc67a16022fb14c02cd190141f6488487016
Uladzislava <santalova.lada25@gmail.com>
2024-11-01 18:24:18 +0100

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const toRead = join(
+    import.meta.dirname,
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+  const stream = createReadStream(toRead);
+  const hashStream = createHash('sha256').setEncoding('hex');
+
+  await pipeline(stream, hashStream, process.stdout);
 };
 
 await calculateHash();
