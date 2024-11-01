f2be84fef4716c258c3793afbce33ace4d485737
Uladzislava <santalova.lada25@gmail.com>
2024-11-01 18:45:19 +0100

    feat: implement 2nd stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+  const toWrite = join(import.meta.dirname, 'files', 'fileToWrite.txt');
+  const stream = createWriteStream(toWrite, 'utf-8');
+
+  process.stdin.pipe(stream);
 };
 
 await write();
