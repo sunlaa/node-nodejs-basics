3a332d5157e82deef1cb1dc245bf472b93ea261c
Uladzislava <santalova.lada25@gmail.com>
2024-11-01 14:50:22 +0100

    feat: implement 4th task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { join } from 'path';
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const toRemove = join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await rm(toRemove);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+    throw new Error(err);
+  }
 };
 
 await remove();
