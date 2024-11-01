1b5b7770dc8f11d9323c190d60510ffc16f2d670
Uladzislava <santalova.lada25@gmail.com>
2024-11-01 15:33:18 +0100

    feat: implement 1st cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+
+  const result = Object.entries(process.env)
+    .filter(([name]) => name.startsWith(prefix))
+    .map(([name, value]) => `${name}=${value}`)
+    .join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
