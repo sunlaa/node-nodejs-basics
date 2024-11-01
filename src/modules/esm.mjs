4b98d53a8063153334bcebb5d2402ec77c5132eb
Uladzislava <santalova.lada25@gmail.com>
2024-11-01 17:44:26 +0100

    feat: implment module task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,41 @@
+import path from 'path';
+import { createRequire } from 'module';
+import { release, version } from 'os';
+import { createServer } from 'http';
+import './files/c.js';
+
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
+const require = createRequire(import.meta.url);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = require('./files/a.json');
+} else {
+  unknownObject = require('./files/b.json');
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServer((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
