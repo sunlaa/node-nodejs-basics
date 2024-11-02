8549d5d183cdfc3fadbe2c51c478a0a6e9f21635
Uladzislava <santalova.lada25@gmail.com>
2024-11-02 15:28:22 +0100

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+
+  const source = createReadStream(
+    join(import.meta.dirname, 'files', 'fileToCompress.txt')
+  );
+  const destination = createWriteStream(
+    join(import.meta.dirname, 'files', 'archive.gz')
+  );
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from 'zlib';
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createGunzip();
+
+  const source = createReadStream(
+    join(import.meta.dirname, 'files', 'archive.gz')
+  );
+  const destination = createWriteStream(
+    join(import.meta.dirname, 'files', 'fileToCompress-1.txt')
+  );
+
+  source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
