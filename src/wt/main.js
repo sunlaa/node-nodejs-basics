dd0143c93dd023c60655c4d8f8d74030dd0e55de
Uladzislava <santalova.lada25@gmail.com>
2024-11-03 18:37:39 +0100

    feat: implement wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const pathToWorker = join(import.meta.dirname, 'worker.js');
+
+  const getResultPromise = (n) =>
+    new Promise((resolve) => {
+      const worker = new Worker(pathToWorker, { workerData: { n } });
+
+      worker.on('message', (value) =>
+        resolve({ data: value, status: 'resolved' })
+      );
+      worker.on('error', () => resolve({ data: null, status: 'error' }));
+    });
+
+  const resultPromises = [];
+  const cores = cpus().length;
+
+  for (let i = 0; i < cores; i++) {
+    resultPromises.push(getResultPromise(10 + i));
+  }
+
+  const results = await Promise.all(resultPromises);
+
+  console.log(results);
 };
 
 await performCalculations();
