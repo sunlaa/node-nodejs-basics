22aa7966e75f680c46890316e2d494a21b32bf58
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-11-04 16:05:00 +0100

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import { join } from 'path';
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToScript = join(import.meta.dirname, 'files', 'script.js');
+
+  fork(pathToScript, args, {stdio: 'inherit'});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['a', 'b', 'c']);
